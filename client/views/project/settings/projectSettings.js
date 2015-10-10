5798f99826fdc6bb4f991401265e7858f972e6cb
hculap <hculap@gmail.com>
2015-10-10 18:23:06 +0200

    project settings

---
+++ client/views/project/settings/projectSettings.js
@@ -0,0 +1,7 @@
+Template.projectSettings.events({
+  'submit [data-action=projectSettings]': function (e) {
+    e.preventDefault();
+    var ProjectId = this.project._id;
+    Projects.update(projectId: {$set: $(e.target).serializeObject()});
+  }
+});
