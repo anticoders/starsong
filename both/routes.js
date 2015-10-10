d60c00ec4b112f672bf6b03cfc3a3a41a62e9d6c
Hubert OG <hubert@orlikarnia.com>
2015-10-10 19:09:01 +0200

    Duplicated tracker

--- both/routes.js
+++ both/routes.js
@@ -1,28 +1,29 @@
 'use strict';
 
 Router.configure({
-  templateNameConverter:          'camelCase',
-  routeControllerNameConverter:   'upperCamelCase',
-  notFoundTemplate:               'notFound',
-  loadingTemplate:                'loading',
-  layoutTemplate:                  'applicationLayout',
+  templateNameConverter:              'camelCase',
+  routeControllerNameConverter:       'upperCamelCase',
+  notFoundTemplate:                   'notFound',
+  loadingTemplate:                    'loading',
+  layoutTemplate:                     'applicationLayout',
 });
 
 Router.map(function() {
 
-  this.route('home',              {path: '/'});
+  this.route('home',                  {path: '/'});
+
+  this.route('onboarding'),           {path: '/onboarding'};
+
+  this.route('project.timeline',      {path: '/timeline/:_id'});
+  this.route('project.settings',      {path: '/settings/:_id'});
+
+
   
   // sandbox 
-  this.route('sandbox.midi',      {path: '/sandbox/midi'});
-
-  this.route('sandbox.player',    {path: '/sandbox/player'});
-
-  this.route('sandbox.recorder',  {path: '/sandbox/recorder'}); 
-
-  this.route('onboarding'),       {path: '/onboarding'};
-
-  this.route('project.timeline', {path: '/timeline/:_id'});
-  this.route('project.settings', {path: '/settings/:_id'});
+  this.route('sandbox.midi',          {path: '/sandbox/midi'});
+  this.route('sandbox.tracker',       {path: '/sandbox/tracker'});
+  this.route('sandbox.player',        {path: '/sandbox/player'});
+  this.route('sandbox.recorder',      {path: '/sandbox/recorder'}); 
 
 
 });
