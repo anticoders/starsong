0dca40ba556a8ff34a5f15a8f8062b9565128ae8
Tomasz Lenarcik <apendua@gmail.com>
2015-10-10 17:07:54 +0200

    Add the favorite Hubert's helper

--- client/app/helpers.js
+++ client/app/helpers.js
@@ -5,3 +5,8 @@ Helpers.registerAs('$');
 Helpers.define('asset', function (path) {
     return Meteor.absoluteUrl() + 'assets' + path;
 });
+
+Helpers.define('equals', function (a, b) {
+   // double equality intentional
+   return a == b;
+});
