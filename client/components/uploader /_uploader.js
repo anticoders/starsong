41d60dd2127c0ad0d0554079c6ecb60fe873b7a6
K-a-s-i-a <grzegorz.pociejewski@gmail.com>
2015-10-10 20:32:58 +0200

    .wav files upload mechanism ready

--- client/components/uploader /_uploader.js
+++ client/components/uploader /_uploader.js
@@ -1,8 +1,9 @@
 
 S3uploader = function(settings){
   this.state    = new ReactiveVar(S3uploader.MODES.READY);
-  this.progress = new ReactiveVar() 
+  this.progress = new ReactiveVar();  
   this.settings = settings || {};
+  this.currentFileUrl = new ReactiveVar(null); 
 };
 
 S3uploader.MODES = {
@@ -12,23 +13,11 @@ S3uploader.MODES = {
 };
 
 _.extend(S3uploader.prototype,{
-  onError : function(err){
-    this.state.set(S3uploader.MODES.ERROR);
-  },
+  onError : function(message){
+    console.log(message); 
+  }, 
   onStart : function(){
-    if(this.settings.onStart){
-      this.settings.onStart();
-    }
-  },
-  onProgress : function(progress,no,code){
-    if(code === 200){
-      this.progressArray[no].loaded.set(true);
-    }
-  },
-  onFinish : function(){
-    if(this.settings && this.settings.onUpload){
-      this.settings.onUpload(null,{fileUrl: this.fileUrl.curValue, fileId: this.fileId.curValue});
-    }
+    console.log("start uploading"); 
   }, 
   createCORSRequest : function(method, url) {
     var xhr;
@@ -43,9 +32,10 @@ _.extend(S3uploader.prototype,{
     }
     return xhr;
   },
-  uploadToS3 : function(file,url,no) {
+  uploadToS3 : function(file,url) {
     var this_s3upload, xhr;
     this_s3upload = this;
+    this_s3upload.progress.set(0); 
     xhr = this.createCORSRequest('PUT', url);
     if (!xhr) {
       this.onError('CORS not supported');
@@ -53,8 +43,8 @@ _.extend(S3uploader.prototype,{
       this_s3upload.onStart();
       xhr.onload = function() {
         if (xhr.status === 200) {
-          this_s3upload.onProgress(100,no, 200);
-          return this_s3upload.onFinish();
+          this_s3upload.state.set(S3uploader.MODES.FINISHED); 
+          return this_s3upload.state.set(S3uploader.MODES.READY);
         } else {
           return this_s3upload.onError('Upload error: ' + xhr.status);
         }
@@ -63,10 +53,11 @@ _.extend(S3uploader.prototype,{
         return this_s3upload.onError('XHR error.');
       };
       xhr.upload.onprogress = function(e) {
+        this_s3upload.state.set(S3uploader.MODES.PROGRESS); 
         var percentLoaded;
         if (e.lengthComputable) {
           percentLoaded = Math.round((e.loaded / e.total) * 100);
-          return this_s3upload.onProgress(percentLoaded,no, percentLoaded === 100 ? 'Finalizing.' : 'Uploading.');
+          return this_s3upload.progress.set(percentLoaded); 
         }
       };
     }
--- server/methods/S3.js
+++ server/methods/S3.js
@@ -2,19 +2,29 @@ AWS = Meteor.npmRequire('aws-sdk');
 
 Meteor.startup(function(){
   AWS.config.update({
-    accessKeyId: "AKIAI7JBDH6XQMVJ5OEQ",
-    secretAccessKey: "V3TVVR5kRSPBuGr3TI0fjFrQZ8ugqBdoOxfJ51uw/pXEo1oipt"
+    accessKeyId: "AKIAI7JBDH6XQMVJ5OEQ", 
+    secretAccessKey: "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", 
+    region: "us-west-2"
   });
 });
 
 Meteor.methods({
 
-  StoreFile : function(data){
+  storeFile : function(data){
     var fileId = S3Files.insert(data); 
     var s3Client = new AWS.S3();
-    var params = {Bucket: "meteor-tracker", Key: fileId };
+    var params = {
+      Bucket: "meteor-tracker", 
+      Key: fileId, 
+      Expires: 600000, 
+      ContentType: 'audio/wav',
+      ACL: 'public-read',
+    };
     var url = s3Client.getSignedUrl('putObject', params);
-    return url;
+    return {
+      url : url, 
+      id  : fileId, 
+    };
   },
 
 });
