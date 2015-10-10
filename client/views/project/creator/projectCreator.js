ac57f4555e6dd9fc3a3378bdc623a3421ddbc648
hculap <hculap@gmail.com>
2015-10-10 22:30:12 +0200

    project creator

---
+++ client/views/project/creator/projectCreator.js
@@ -0,0 +1,8 @@
+Template.projectCreator.events({
+  'submit [data-action=create]': function (e) {
+    e.preventDefault();
+    var project = $(e.target).serializeObject();
+    var projectId = Projects.insert(project);
+    Router.go('project.timeline',{_id: projectId});
+  }
+});
