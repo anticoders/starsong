2d65d56fe0e1ce2fc5a84e28d406fa65a39d03e4
Hubert OG <hubert@orlikarnia.com>
2015-10-10 18:46:13 +0200

    Background

--- client/views/project/settings/projectSettings.js
+++ client/views/project/settings/projectSettings.js
@@ -2,6 +2,9 @@ Template.projectSettings.events({
   'submit [data-action=projectSettings]': function (e) {
     e.preventDefault();
     var ProjectId = this.project._id;
-    Projects.update(projectId: {$set: $(e.target).serializeObject()});
-  }
+    // Projects.update(projectId, {$set: $(e.target).serializeObject()});
+  },
+
 });
+
+
