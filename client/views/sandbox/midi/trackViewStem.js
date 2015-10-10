1d1afb211d1818099e2b3ac0e480a661ea59b6bb
Hubert OG <hubert@orlikarnia.com>
2015-10-10 18:17:11 +0200

    Stem saving

--- client/views/sandbox/midi/trackViewStem.js
+++ client/views/sandbox/midi/trackViewStem.js
@@ -1,19 +1,25 @@
 
 Template.trackViewStem.rendered = function() {
-  this.$('.mRightDrag .pep').pep({
+  var self = this;
+
+  self.$('.mRightDrag').pep({
     axis: 'x',
     start: function(ev, obj) {
       var stem = obj.$el.closest('.mStem');
+      stem.addClass('pepActive');
       stem.data('pepinitwidth', stem.width() - ev.screenX);
     },
     drag: function(ev, obj) {
-      // console.log("ev.", ev);
-      // console.log("---", stem.data('pepinitwidth'), ev.offsetX, ev.pageX, ev.screenX);
       var stem = obj.$el.closest('.mStem');
       stem.css('width', stem.data('pepinitwidth') + ev.screenX + 'px');
     },
     stop: function(ev, obj) {
-      // console.log("STOP", obj);
+      var stem = obj.$el.closest('.mStem');
+      stem.removeClass('pepActive');
+
+      Stems.update(self.data._id, {$set: {
+        x1: self.data.x0 + Utils.music.pxToTime(stem.width()),
+      }});
     },
   });
 };
@@ -23,11 +29,11 @@ Template.trackViewStem.rendered = function() {
 Template.trackViewStem.helpers({
 
   posX0: function() {
-    return Math.floor(this.x0 * 100 / Utils.music.second);
+    return Utils.music.timeToPx(this.x0);
   },
 
   posW: function() {
-    return Math.floor((this.x1 - this.x0) * 100 / Utils.music.second);
+    return Utils.music.timeToPx(this.x1 - this.x0);
   },
 
 });
