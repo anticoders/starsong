a584d0c4802bc4c620afe47563978744b239e207
hculap <hculap@gmail.com>
2015-10-11 12:09:54 +0200

    menu

--- client/views/home/_homeController.js
+++ client/views/home/_homeController.js
@@ -1,21 +1,6 @@
 HomeController = ApplicationController.extend({
 
-  waitOn: function() {
-    return [
-      Meteor.subscribe('projects.mine'),
-    ];
-  },
 
-  data: function() {
-    var data = {
-      projects: Projects.find({}, {
-        sort: {name: 1},
-      }),
-    };
-
-    __data = data;
-    return data;
-  },
 
 });
 
