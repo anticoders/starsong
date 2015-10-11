39f59ed08390deb15406d281d417d2c1dd4f2a58
Hubert OG <hubert@orlikarnia.com>
2015-10-11 12:33:33 +0200

    Dragging timeline

--- client/components/mTracker/mTrackerStemView.js
+++ client/components/mTracker/mTrackerStemView.js
@@ -9,23 +9,94 @@ Template.mTrackerStemView.rendered = function() {
     place:                false,
     useCSSTranslation:    false,
     start: function(ev, obj) {
-      var stem = obj.$el.closest('.mStem');
-      stem.addClass('pepActive');
-      stem.data('pepinitwidth', stem.width() - ev.screenX);
+      var rect = obj.$el.closest('.mStem');
+      rect.addClass('pepActive');
+      rect.data('pepinitwidth', rect.width() - ev.screenX);
     },
     drag: function(ev, obj) {
-      var stem = obj.$el.closest('.mStem');
-      stem.css('width', stem.data('pepinitwidth') + ev.screenX + 'px');
+      var rect = obj.$el.closest('.mStem');
+      rect.css('width', rect.data('pepinitwidth') + ev.screenX + 'px');
     },
     stop: function(ev, obj) {
-      var stem = obj.$el.closest('.mStem');
-      stem.removeClass('pepActive');
+      var rect = obj.$el.closest('.mStem');
+      rect.removeClass('pepActive');
 
       Stems.update(self.data._id, {$set: {
-        x1: self.data.x0 + Utils.music.pxToTime(stem.width()),
+        x1: Utils.music.pxToTime(rect.left() + rect.width() - 20),
       }});
 
       // obj.$el.css('translation', 'none');
+    },
+  });
+
+
+
+  self.$('.mLeftDrag').pep({
+    axis:                 'x',
+    place:                false,
+    useCSSTranslation:    false,
+    droppable:            '.mTrack',
+    start: function(ev, obj) {
+      var rect = obj.$el.closest('.mStem');
+      rect.addClass('pepActive');
+      rect.data('pepinitleft', rect.left() - ev.screenX);
+      // rect.data('pepinittop', rect.top() - ev.screenY);
+    },
+    drag: function(ev, obj) {
+      var rect = obj.$el.closest('.mStem');
+      rect.css({
+        left: rect.data('pepinitleft') + ev.screenX + 'px',
+        // top: rect.data('pepinittop') + ev.screenY + 'px',
+      });
+      obj.$el.css({
+        left: '-20px',
+        // top:  '0px',
+      });
+    },
+    stop: function(ev, obj) {
+      var rect = obj.$el.closest('.mStem');
+      rect.removeClass('pepActive');
+
+      Stems.update(self.data._id, {$set: {
+        x0: Utils.music.pxToTime(rect.left() - 20),
+        x1: Utils.music.pxToTime(rect.left() + rect.width() - 20),
+      }});
+
+      obj.$el.css({
+        left: '-20px',
+        // top:  '0px',
+      });
+
+      // var drop = this.activeDropRegions[0];
+
+      // if(!drop) {
+      //   // Stems.update(stemOb._id, {$set: _.object(
+      //   //   ['midi.' + noteOb.idx + '.delete'],
+      //   //   [true]
+      //   // )});
+      //   Stems.update(stemOb._id, {$pull: {
+      //     midi: {_id: noteOb._id},
+      //   }});
+
+      //   return;
+      // }
+
+      // Stems.update(stemOb._id, {$set: _.object(
+      //   ['midi.' + noteOb.idx + '.t0',
+      //    'midi.' + noteOb.idx + '.t1',
+      //    'midi.' + noteOb.idx + '.n',
+      //   ],
+      //   [Utils.music.pxToTime(note.left() - 20),
+      //    Utils.music.pxToTime(note.left() + note.width() - 20),
+      //    drop.data('note'),
+      //   ]
+      // )});
+
+
+      // $('.pep-dpa').removeClass('pep-dpa');
+      // note.css({
+      //   top: '1px',
+      // });
     },
   });
 };
